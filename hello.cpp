176f19d805586584e9f8287643cf49554768ec55
matteospatola <matteospatola@hotmail.com>
2023-02-07 13:29:01 -0500

    Remove Div3()

--- hello.cpp
+++ hello.cpp
@@ -2,11 +2,9 @@
 
 int Mult(int a, int b) {return a*b;}
 
-double Div(double a, double b) {return a/b;}
    
 int main()
 {
     std::cout << Mult(10,3) << std::endl;
-    std::cout << Div(5,2) << std::endl;
 
 }
