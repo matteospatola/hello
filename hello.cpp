744b3da348592b18eed5210f3055236baba2396e
matteospatola <111610098+matteospatola@users.noreply.github.com>
2023-02-06 22:55:44 -0500

    Add Div()

--- hello.cpp
+++ hello.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
-int Mult(int a, int b)
-{
-    return a*b;
-}
+int Mult(int a, int b) {return a*b;}
 
+double Div(double a, double b) {return a/b;}
+   
 int main()
 {
     std::cout << "Matteo" << std::endl;
     std::cout << Mult(10,3) << std::endl;
+    std::cout << Div(5,2) << std::endl;
 
 }
