8b5eda3ad38810de4f1a97cde23edc9c2362e3b2
matteospatola <matteospatola@hotmail.com>
2023-02-06 23:08:58 -0500

    Changed first line inside main function

--- hello.cpp
+++ hello.cpp
@@ -6,7 +6,7 @@ double Div(double a, double b) {return a/b;}
    
 int main()
 {
-    std::cout << "Matteo" << std::endl;
+    std::cout << "Spatola" << std::endl;
     std::cout << Mult(10,3) << std::endl;
     std::cout << Div(5,2) << std::endl;
 
